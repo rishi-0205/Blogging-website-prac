e14d7fc49a109a5eaa9b89d5206b9f40293f87a0
rishi-0205 <rishijc02@gmail.com>
2024-05-18 23:21:43 +0530

    Added Signout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,28 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
   const path = useLocation().pathName;
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
   const dispatch = useDispatch();
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Navbar className="border-b-2">
       <Link
@@ -46,7 +62,9 @@ export default function Header() {
           <Dropdown
             arrowIcon={false}
             inline
-            label={<Avatar alt="user" img="#" rounded />}
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
           >
             <Dropdown.Header>
               <span className="block text-sm">@{currentUser.username}</span>
@@ -58,6 +76,7 @@ export default function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in" outline="true">
@@ -80,5 +99,3 @@ export default function Header() {
     </Navbar>
   );
 }
-
-//{currentUser.profilePicture}
