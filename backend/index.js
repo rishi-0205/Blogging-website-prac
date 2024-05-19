165d2512c34d0e5a10a47d30f22c75cc3164e5a0
rishi-0205 <rishijc02@gmail.com>
2024-05-19 17:34:15 +0530

    Created an API route for posting blog

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -35,6 +36,8 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
