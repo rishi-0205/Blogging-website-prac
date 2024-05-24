20ac09ea1e6e3ea83581aa308e4b53e68ecfe4a6
rishi-0205 <rishijc02@gmail.com>
2024-05-24 23:02:37 +0530

    Added comment viewing area to post page

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signout,
   updateUser,
@@ -12,5 +13,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
