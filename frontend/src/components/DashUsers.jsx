89aeb4927a41094cf0b41f19fceb10a1c76ef900
rishi-0205 <rishijc02@gmail.com>
2024-05-24 12:15:45 +0530

    Added Delete API route

--- frontend/src/components/DashUsers.jsx
+++ frontend/src/components/DashUsers.jsx
@@ -45,25 +45,20 @@ export default function DashUsers() {
     }
   };
   const handleDeleteUser = async () => {
-    //   setShowModal(false);
-    //   try {
-    //     const res = await fetch(
-    //       `/api/user/deleteuser/${userIdToDelete}/${currentUser._id}`,
-    //       {
-    //         method: "DELETE",
-    //       }
-    //     );
-    //     const data = await res.json();
-    //     if (!res.ok) {
-    //       console.log(data.message);
-    //     } else {
-    //       setUserPosts((prev) =>
-    //         prev.filter((post) => post._id !== postIdToDelete)
-    //       );
-    //     }
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
+    setShowModal(false);
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thunb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thunb-slate-500">
