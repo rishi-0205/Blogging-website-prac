b5982b7577514aa0f0805e7c3f6d97031d231e8a
rishi-0205 <rishijc02@gmail.com>
2024-05-26 17:24:13 +0530

    Added Search page functionality, completed about and projects page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
-import FooterCom from "./components/Footer";
+import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
-import PostsPage from "./pages/PostsPage";
+import PostPage from "./pages/PostsPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -21,21 +21,23 @@ export default function App() {
       <ScrollToTop />
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path="/create-post" element={<CreatePost />}></Route>
-          <Route path="/update-post/:postId" element={<UpdatePost />}></Route>
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/post/:postSlug" element={<PostsPage />}></Route>
+
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
       </Routes>
-      <FooterCom />
+      <Footer />
     </BrowserRouter>
   );
 }
--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,19 +1,23 @@
 import { Sidebar } from "flowbite-react";
-import React, { useEffect, useState } from "react";
 import {
-  HiAnnotation,
+  HiUser,
   HiArrowSmRight,
   HiDocumentText,
   HiOutlineUserGroup,
-  HiUser,
+  HiAnnotation,
+  HiChartPie,
 } from "react-icons/hi";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 export default function DashSidebar() {
   const location = useLocation();
-  const [tab, setTab] = useState("");
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -21,17 +25,43 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
+          {currentUser && currentUser.isAdmin && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item
+                active={tab === "dash" || !tab}
+                icon={HiChartPie}
+                as="div"
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
               label={currentUser.isAdmin ? "Admin" : "User"}
               labelColor="dark"
-              as={"div"}
+              as="div"
             >
               Profile
             </Sidebar.Item>
@@ -39,40 +69,41 @@ export default function DashSidebar() {
           {currentUser.isAdmin && (
             <Link to="/dashboard?tab=posts">
               <Sidebar.Item
-                active={tab === "post"}
+                active={tab === "posts"}
                 icon={HiDocumentText}
-                labelColor="dark"
-                as={"div"}
+                as="div"
               >
                 Posts
               </Sidebar.Item>
             </Link>
           )}
           {currentUser.isAdmin && (
-            <Link to="/dashboard?tab=users">
-              <Sidebar.Item
-                active={tab === "users"}
-                icon={HiOutlineUserGroup}
-                labelColor="dark"
-                as={"div"}
-              >
-                Users
-              </Sidebar.Item>
-            </Link>
+            <>
+              <Link to="/dashboard?tab=users">
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiOutlineUserGroup}
+                  as="div"
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to="/dashboard?tab=comments">
+                <Sidebar.Item
+                  active={tab === "comments"}
+                  icon={HiAnnotation}
+                  as="div"
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
-          {currentUser.isAdmin && (
-            <Link to="/dashboard?tab=comments">
-              <Sidebar.Item
-                active={tab === "comments"}
-                icon={HiAnnotation}
-                labelColor="dark"
-                as={"div"}
-              >
-                Comments
-              </Sidebar.Item>
-            </Link>
-          )}
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
